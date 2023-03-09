f96fb0f00a0cc93e2997536291ac86c82c08ccce
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-03-09 09:30:31 -0400

    Agregando la funcionalidad de publicar el post

--- src/presenter.js
+++ src/presenter.js
@@ -1,13 +1,53 @@
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+let ListaPosts = [];
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+const  ObjPost= {
+  fecha: "",
+  titulo: "",
+  texto: "",
+};
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+const form = document.querySelector("#form");
+const InsertTittle = document.querySelector("#titulo");
+const InsertText = document.querySelector("#texto");
+const botonPublicar = document.querySelector("#btn-publicar");
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-});
+function crearPost() 
+{
+    ListaPosts.push({ ...ObjPost });
+    mostrarPosts();
+    form.reset();
+    limpiarPost();
+}
+
+
+function limpiarPost() 
+{
+    ObjPost.fecha = "";
+    ObjPost.titulo = "";
+    ObjPost.texto = "";
+}
+
+function refreshHTML() {
+  const divPosts = document.querySelector(".div-posts");
+  while (divPosts.firstChild) {
+    divPosts.removeChild(divPosts.firstChild);
+  }
+}
+
+function mostrarPosts() {
+  refreshHTML();
+  const divPosts = document.querySelector(".div-posts");
+
+  ListaPosts.forEach((nota) => {
+    const { fecha, titulo, texto } = nota;
+
+    const parrafo = document.createElement("p");
+    parrafo.textContent = `${fecha} - ${titulo} - ${texto} - `;
+    parrafo.dataset.id = fecha;
+
+    const hr = document.createElement("hr");
+    divPosts.appendChild(parrafo);
+    divPosts.appendChild(hr);
+  });
+  }
+;
